66bfe092749e8043434e36e3eade5e5132d50948
Angel Serrano <131713028+Angelserpra@users.noreply.github.com>
2024-06-14 15:19:34 +0200

    Update precios.js

--- htdocs/dcs/java/precios.js
+++ htdocs/dcs/java/precios.js
@@ -1,12 +1,8 @@
-// script.js
 function selectOption(element) {
-    // Remover la clase 'selected' de todos los elementos
     var pricingCards = document.querySelectorAll('.pricing-card');
     pricingCards.forEach(function(card) {
       card.classList.remove('selected');
     });
-  
-    // Agregar la clase 'selected' al elemento seleccionado
     element.classList.add('selected');
   }
   
